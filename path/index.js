83e004285bc0835edeb3d950b09d333f6a232a99
Saman <samanezzatabadi@yahoo.com>
2025-10-27 13:30:36 +0330

    path prac

---
+++ path/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+// windows => C:/Public/App
+// linux => apt/user/...
+// mac => apt/user/...
+
+// console.log("mongodb" + path.sep + "path" + path.sep + "users");
+// console.log("mongodb" + path.delimiter + "path" + path.delimiter + "users");
+
+// console.log(path.basename("mongodb/server.js", ".js"));
+// console.log(path.dirname("mongodb/server.js"));
+
+// console.log(path.extname("mongodb/server.js"));
+// console.log(path.extname("app.txt"));
+// console.log(path.extname("app.mp4"));
+// console.log(path.extname("app.pbg"));
+
+// console.log(path.join("/", "mongodb", "path", "users"));
+// console.log(path.isAbsolute(path.join("/", "mongodb", "path", "users")));
+
+// console.log(path.parse("mongodb/server.js"));
+// log
+// {
+//   root: '',
+//   dir: 'mongodb',
+//   base: 'server.js',
+//   ext: '.js',
+//   name: 'server'
+// }
+
+// console.log(path.normalize("mongodb\\lorem/server.js"));
+
+// console.log(__dirname);
+// console.log(__filename);
