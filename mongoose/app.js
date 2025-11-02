c18028d977b3ae337b0e20f122172ec50d7b9be1
Saman <samanezzatabadi@yahoo.com>
2025-11-02 13:09:06 +0330

    worked with mongoose

--- mongoose/app.js
+++ mongoose/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { ErrorHandler, NotFoundError } = require("./util/errorHandlers");
 const BlogModel = require("./model/blog.model");
+const { isValidObjectId } = require("mongoose");
+const omitEmpty = require("omit-empty");
 require("./config/mongo.config");
 const app = express();
 
@@ -15,6 +17,161 @@ app.post("/blogs", async (req, res, next) => {
       title,
       text,
     });
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+app.post("/new", async (req, res, next) => {
+  const { title, text } = req.body;
+
+  try {
+    const newBlog = new BlogModel({
+      title,
+      text,
+    });
+
+    await newBlog.save();
+
+    res.send(newBlog);
+  } catch (error) {
+    next(error);
+  }
+});
+app.get("/insertManyBlog", async (req, res, next) => {
+  try {
+    const result = await BlogModel.insertMany([
+      {
+        title: "title3",
+        text: "text3",
+      },
+      {
+        title: "title4",
+        text: "text4",
+      },
+      {
+        title: "title5",
+        text: "text5",
+      },
+    ]);
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+app.get("/blogs", async (req, res, next) => {
+  try {
+    const blogs = await BlogModel.find();
+
+    res.json({
+      status: 200,
+      data: blogs,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+app.get("/blogs/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    throw {
+      status: 400,
+      message: "invalid id",
+    };
+  }
+
+  try {
+    const result = await BlogModel.findById({
+      _id: id,
+    });
+    if (!result) {
+      throw {
+        status: 404,
+        message: "blog not found",
+      };
+    }
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+app.delete("/blogs/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    throw {
+      status: 400,
+      message: "invalid id",
+    };
+  }
+
+  try {
+    const result = await BlogModel.deleteOne({
+      _id: id,
+    });
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+app.delete("/blogs", async (req, res, next) => {
+  try {
+    const result = await BlogModel.deleteMany();
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+app.put("/blogs/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    throw {
+      status: 400,
+      message: "invalid id",
+    };
+  }
+
+  // OPTION 1
+
+  // try {
+  //   const newBodyObject = omitEmpty(req.body);
+  //   const blog = await BlogModel.findById({
+  //     _id: id,
+  //   });
+  //   if (!blog) {
+  //     throw {
+  //       status: 404,
+  //       message: "blog not found",
+  //     };
+  //   }
+  //   console.log(blog);
+  //   Object.assign(blog, newBodyObject);
+  //   await blog.save();
+
+  //   res.send(blog);
+  // } catch (error) {
+  //   next(error);
+  // }
+
+  // OPTION 2
+  try {
+    const newBodyObject = omitEmpty(req.body);
+
+    const result = await BlogModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: newBodyObject,
+      }
+    );
+
     res.send(result);
   } catch (error) {
     next(error);
